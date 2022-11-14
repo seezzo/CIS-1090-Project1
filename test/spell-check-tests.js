3d26ceddc6132195669a89998d4cef6bc14cbbf5
Test Student <cuteststudent@billkuker.com>
2022-11-14 08:48:27 -0500

    commenting

--- test/spell-check-tests.js
+++ test/spell-check-tests.js
@@ -3,6 +3,7 @@ import assert from "assert"; //Utilities to help the test
 
 
 //These tests us a framework named Mocha https://mochajs.org/#getting-started
+// "describe" and "it" are part of the mocha framework.
 
 describe("SpellCheck tests", function () {
 
@@ -22,11 +23,11 @@ describe("SpellCheck tests", function () {
 
     describe("Spell Check Tests", function () {
 
-        it("Word banana is spelled right", function () {
+        it("Reports that banana is spelled right", function () {
             assert.ok(isSpelledRight("banana"));
         });
 
-        it("Word Pikachu is misspelled", function () {
+        it("Reports that Pikachu is misspelled", function () {
             assert.ok(!isSpelledRight("Pikachu"));
         });
 
